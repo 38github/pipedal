c78b6c6d2245cd668525afa318e62f2ee4343108
38github <18294149+38github@users.noreply.github.com>
2023-09-28 22:36:50 +0200

    Update init.sh
    test
    
    Signed-off-by: 38github <18294149+38github@users.noreply.github.com>

--- init.sh
+++ init.sh
@@ -2,6 +2,9 @@
 
 # Configure cmake build.
 echo Configuring cmake build...
+export CFLAGS="-O2 -flto=1 -ftree-parallelize-loops=4 -floop-parallelize-all -fopenmp"
+export CXXFLAGS="-O2 -flto=1 -ftree-parallelize-loops=4 -floop-parallelize-all -fopenmp"
+export LDFLAGS="-O2 -flto=1 -ftree-parallelize-loops=4 -floop-parallelize-all -fopenmp"
 mkdir build
 cd build
 cmake ..
