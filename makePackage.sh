769e3ee814a6d8ca0900b18600d0b18434e4bf6f
38github <18294149+38github@users.noreply.github.com>
2023-09-28 22:37:19 +0200

    Update makePackage.sh
    test
    
    Signed-off-by: 38github <18294149+38github@users.noreply.github.com>

--- makePackage.sh
+++ makePackage.sh
@@ -1,3 +1,6 @@
+export CFLAGS="-O2 -flto=1 -ftree-parallelize-loops=4 -floop-parallelize-all -fopenmp"
+export CXXFLAGS="-O2 -flto=1 -ftree-parallelize-loops=4 -floop-parallelize-all -fopenmp"
+export LDFLAGS="-O2 -flto=1 -ftree-parallelize-loops=4 -floop-parallelize-all -fopenmp"
 cd build
 cpack -G DEB -C Release -config CPackConfig.cmake 
 cd ..
