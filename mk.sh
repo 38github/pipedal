0c49ce050f41be9ddf1eb245ae0c30e1a2d8b1a4
38github <18294149+38github@users.noreply.github.com>
2023-09-28 22:37:50 +0200

    Update mk.sh
    test
    
    Signed-off-by: 38github <18294149+38github@users.noreply.github.com>

--- mk.sh
+++ mk.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/bash
+export CFLAGS="-O2 -flto=1 -ftree-parallelize-loops=4 -floop-parallelize-all -fopenmp"
+export CXXFLAGS="-O2 -flto=1 -ftree-parallelize-loops=4 -floop-parallelize-all -fopenmp"
+export LDFLAGS="-O2 -flto=1 -ftree-parallelize-loops=4 -floop-parallelize-all -fopenmp"
 /usr/bin/cmake --build build --config RelWithDebInfo --target all 
